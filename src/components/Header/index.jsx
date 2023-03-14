83e9cea0795c767789b2e1a756187227600b29a5
sitilyGITHUB <ggwp40913@gmail.com>
2023-03-14 23:38:54 +0600

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//styles
+import './Header.scss'
+
+const Header = () => {
+  return (
+    <div className='header'>Header</div>
+  )
+}
+
+export default Header
---
+++ src/components/Main/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//styles
+import './Main.scss'
+
+const Main = () => {
+  return (
+    <div>Main</div>
+  )
+}
+
+export default Main
